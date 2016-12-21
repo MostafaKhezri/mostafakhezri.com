e2bd56d0c6955ca7c5b80479837770fe6a0f425c
Mostafa Khezri <mostafanodet@gmail.com>
2016-12-21 10:37:51 -0800

    Initial commit

---
+++ photoblog/links-crsl-flowers.js
@@ -0,0 +1,30 @@
+document.write('\
+<div id="links-crsl-flowers">\
+    <a href="flowers/pics/IMG_0041.JPG" title=""><img src="flowers/pics/IMG_0041.JPG" alt="IMG_0041" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_0084.JPG" title=""><img src="flowers/pics/IMG_0084.JPG" alt="IMG_0084" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_0177.JPG" title=""><img src="flowers/pics/IMG_0177.JPG" alt="IMG_0177" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_0189.JPG" title=""><img src="flowers/pics/IMG_0189.JPG" alt="IMG_0189" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_0404.JPG" title=""><img src="flowers/pics/IMG_0404.JPG" alt="IMG_0404" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_1006.JPG" title=""><img src="flowers/pics/IMG_1006.JPG" alt="IMG_1006" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_2560.JPG" title=""><img src="flowers/pics/IMG_2560.JPG" alt="IMG_2560" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_3019.JPG" title=""><img src="flowers/pics/IMG_3019.JPG" alt="IMG_3019" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_3859.JPG" title=""><img src="flowers/pics/IMG_3859.JPG" alt="IMG_3859" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_3862.JPG" title=""><img src="flowers/pics/IMG_3862.JPG" alt="IMG_3862" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_3864.JPG" title=""><img src="flowers/pics/IMG_3864.JPG" alt="IMG_3864" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_4486.JPG" title=""><img src="flowers/pics/IMG_4486.JPG" alt="IMG_4486" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_4934.JPG" title=""><img src="flowers/pics/IMG_4934.JPG" alt="IMG_4934" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_4935.JPG" title=""><img src="flowers/pics/IMG_4935.JPG" alt="IMG_4935" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_4937.JPG" title=""><img src="flowers/pics/IMG_4937.JPG" alt="IMG_4937" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_4938.JPG" title=""><img src="flowers/pics/IMG_4938.JPG" alt="IMG_4938" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_4939.JPG" title=""><img src="flowers/pics/IMG_4939.JPG" alt="IMG_4939" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_4942.JPG" title=""><img src="flowers/pics/IMG_4942.JPG" alt="IMG_4942" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_5480.JPG" title=""><img src="flowers/pics/IMG_5480.JPG" alt="IMG_5480" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_5481.JPG" title=""><img src="flowers/pics/IMG_5481.JPG" alt="IMG_5481" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_5761.JPG" title=""><img src="flowers/pics/IMG_5761.JPG" alt="IMG_5761" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_5764.JPG" title=""><img src="flowers/pics/IMG_5764.JPG" alt="IMG_5764" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_5783.JPG" title=""><img src="flowers/pics/IMG_5783.JPG" alt="IMG_5783" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_0045.JPG" title=""><img src="flowers/pics/IMG_0045.JPG" alt="IMG_0045" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_5784.JPG" title=""><img src="flowers/pics/IMG_5784.JPG" alt="IMG_5784" style="width:160px;height:120px"></a>\
+    <a href="flowers/pics/IMG_5785.JPG" title=""><img src="flowers/pics/IMG_5785.JPG" alt="IMG_5785" style="width:160px;height:120px"></a>\
+</div>\
+');
---
+++ photoblog/links-crsl-otherangle.js
@@ -0,0 +1,42 @@
+document.write('\
+<div id="links-crsl-otherangle">\
+    <a href="otherangle/pics/IMG_0129.jpg" title=""><img src="otherangle/pics/IMG_0129.jpg" alt="IMG_0129" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0130.jpg" title=""><img src="otherangle/pics/IMG_0130.jpg" alt="IMG_0130" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0134.jpg" title=""><img src="otherangle/pics/IMG_0134.jpg" alt="IMG_0134" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0135.jpg" title=""><img src="otherangle/pics/IMG_0135.jpg" alt="IMG_0135" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0139.jpg" title=""><img src="otherangle/pics/IMG_0139.jpg" alt="IMG_0139" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0151.jpg" title=""><img src="otherangle/pics/IMG_0151.jpg" alt="IMG_0151" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0154.jpg" title=""><img src="otherangle/pics/IMG_0154.jpg" alt="IMG_0154" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0241.jpg" title=""><img src="otherangle/pics/IMG_0241.jpg" alt="IMG_0241" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0252.jpg" title=""><img src="otherangle/pics/IMG_0252.jpg" alt="IMG_0252" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0262.jpg" title=""><img src="otherangle/pics/IMG_0262.jpg" alt="IMG_0262" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0286.jpg" title=""><img src="otherangle/pics/IMG_0286.jpg" alt="IMG_0286" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0296.jpg" title=""><img src="otherangle/pics/IMG_0296.jpg" alt="IMG_0296" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0299.jpg" title=""><img src="otherangle/pics/IMG_0299.jpg" alt="IMG_0299" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0302.jpg" title=""><img src="otherangle/pics/IMG_0302.jpg" alt="IMG_0302" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0304.jpg" title=""><img src="otherangle/pics/IMG_0304.jpg" alt="IMG_0304" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0314.jpg" title=""><img src="otherangle/pics/IMG_0314.jpg" alt="IMG_0314" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0326.jpg" title=""><img src="otherangle/pics/IMG_0326.jpg" alt="IMG_0326" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0356.jpg" title=""><img src="otherangle/pics/IMG_0356.jpg" alt="IMG_0356" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0359.jpg" title=""><img src="otherangle/pics/IMG_0359.jpg" alt="IMG_0359" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0534.jpg" title=""><img src="otherangle/pics/IMG_0534.jpg" alt="IMG_0534" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0568.jpg" title=""><img src="otherangle/pics/IMG_0568.jpg" alt="IMG_0568" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0716.jpg" title=""><img src="otherangle/pics/IMG_0716.jpg" alt="IMG_0716" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0728.jpg" title=""><img src="otherangle/pics/IMG_0728.jpg" alt="IMG_0728" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_0733.jpg" title=""><img src="otherangle/pics/IMG_0733.jpg" alt="IMG_0733" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_1263.jpg" title=""><img src="otherangle/pics/IMG_1263.jpg" alt="IMG_1263" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_1278.jpg" title=""><img src="otherangle/pics/IMG_1278.jpg" alt="IMG_1278" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_1375.jpg" title=""><img src="otherangle/pics/IMG_1375.jpg" alt="IMG_1375" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_1379.jpg" title=""><img src="otherangle/pics/IMG_1379.jpg" alt="IMG_1379" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_1399.jpg" title=""><img src="otherangle/pics/IMG_1399.jpg" alt="IMG_1399" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_3460.jpg" title=""><img src="otherangle/pics/IMG_3460.jpg" alt="IMG_3460" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_3466.jpg" title=""><img src="otherangle/pics/IMG_3466.jpg" alt="IMG_3466" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_3471.jpg" title=""><img src="otherangle/pics/IMG_3471.jpg" alt="IMG_3471" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_3616.jpg" title=""><img src="otherangle/pics/IMG_3616.jpg" alt="IMG_3616" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_3628.jpg" title=""><img src="otherangle/pics/IMG_3628.jpg" alt="IMG_3628" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_3632.jpg" title=""><img src="otherangle/pics/IMG_3632.jpg" alt="IMG_3632" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_3739.jpg" title=""><img src="otherangle/pics/IMG_3739.jpg" alt="IMG_3739" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_3763.jpg" title=""><img src="otherangle/pics/IMG_3763.jpg" alt="IMG_3763" style="width:160px;height:120px"></a>\
+    <a href="otherangle/pics/IMG_3851.jpg" title=""><img src="otherangle/pics/IMG_3851.jpg" alt="IMG_3851" style="width:160px;height:120px"></a>\
+</div>\
+');
